a1b5aa0276d76c1491f07d9a508cc4efaae9091f
noahgerard <helix.dev.xyz@gmail.com>
2024-09-28 03:14:50 -0400

    Added jotai state manager

--- frontend/src/app/claim/page.tsx
+++ frontend/src/app/claim/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { useState } from 'react';
+import { claimStageAtom } from '../lib/state';
+import { useAtom } from 'jotai';
 
 function Stage1() {
+	const [stage, setStage] = useAtom(claimStageAtom);
+
 	return <div className='flex flex-col gap-8 p-4'>
 		<p className='text-2xl'><strong>Step 1</strong></p>
 
@@ -14,18 +18,19 @@ function Stage1() {
 		</h2>
 
 		<div className='flex flex-row gap-2'>
-			<button className='btn flex-1 btn-success'>Yes</button>
-			<button className='btn flex-1 btn-error'>No</button>
+			<button className='btn flex-1 btn-success' onClick={() => setStage(2)}>Yes</button>
+			<button className='btn flex-1 btn-error' onClick={() => setStage(1.5)}>No</button>
 		</div>
 	</div>
 }
 
 export default function ClaimPage() {
+	const [stage, setStage] = useAtom(claimStageAtom);
+
 	return <>
 		<div className='w-full px-4 py-10 rounded-md flex-1 flex justify-center'>
 			<div className='max-w-[25rem]'>
 				<Stage1 />
-
 			</div>
 		</div>
 	</>
