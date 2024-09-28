fda4e8857005d595ea450b9130c20a5cd3e266d1
must108 <136145181+must108@users.noreply.github.com>
2024-09-28 04:09:05 -0400

    add gpt prompting

---
+++ backend/test.py
@@ -0,0 +1,34 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+prompt = f"""
+Based on this users info and the picture of the cars damages, estimate and return repair_cost, claim_amount, deductible_amount, monthly_premium_increase in a JSON format, do not return anything else in your response:
+
+Personal Info
+Address: 11200 SW 8th St, Miami, FL 33199 
+Age: 32
+Gender: Male
+
+Vehicle Information
+Make: Toyota
+Model: Camry
+Year: 2018
+Millage: 100000
+
+Insurance
+Insurance Company: State Farm
+Deductible Amount: $500
+Premium Details: Monthly $100
+
+Reminder: do not return anything else in your response outside of the JSON
+"""
+
+res = llm.invoke(prompt)
+print(res)
