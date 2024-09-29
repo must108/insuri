41595bd2789c918b006b09a3ba58b2fca033f31d
noahgerard <helix.dev.xyz@gmail.com>
2024-09-29 08:22:35 -0400

    Refactor Home page layout for improved mobile responsiveness and user experience

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -45,18 +45,18 @@ const items = [
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center p-8 pb-20 gap-12 sm:p-20 min-h-screen max-w-5xl mx-auto">
+      <div className="flex flex-col items-center justify-center p-8 md:pb-20 gap-8 md:gap-12 sm:p-20 min-h-screen max-w-5xl mx-auto">
 
         <div className="flex flex-col gap-4">
-          <p className="text-5xl font-bold text-center">
+          <p className="text-3xl md:text-5xl font-bold text-center">
             Simplified Auto Insurance Claims
           </p>
-          <p className="text-xl text-center">
+          <p className="text-lg md:text-xl text-center">
             Streamline your auto insurance claims and master your insurance journey with our easy-to-use platform. Trust us to help you get back on the road faster.
           </p>
         </div>
 
-        <div className="flex w-fit items-center justify-center gap-4 btn-primary">
+        <div className="flex w-fit flex-wrap items-center justify-center gap-4 btn-primary">
           <FileClaimButton />
           <LearnMoreButton />
         </div>
